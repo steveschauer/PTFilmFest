0a774da9abb30f4b382e387554118c4cd5b2ac13
Steve Schauer <steve@bloc.io>
2015-08-18 06:08:36 -0700

    work in progress

---
+++ PTFilmFest/PTFilmFest/CircleSpinnerView.h
@@ -0,0 +1,15 @@
+//
+//  CircleSpinnerView.h
+//  Blocstagram
+//
+//  Created by Steve Schauer on 8/2/15.
+//  Copyright (c) 2015 Steve Schauer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CircleSpinnerView : UIView
+@property (nonatomic, assign) CGFloat strokeThickness;
+@property (nonatomic, assign) CGFloat radius;
+@property (nonatomic, strong) UIColor *strokeColor;
+@end
---
+++ PTFilmFest/PTFilmFest/PTFilmFest_Bridging_Header.h
@@ -0,0 +1,13 @@
+//
+//  PTFilmFest_Bridging_Header.h
+//  PTFilmFest
+//
+//  Created by Steve Schauer on 8/13/15.
+//  Copyright (c) 2015 Steve Schauer. All rights reserved.
+//
+
+#ifndef PTFilmFest_PTFilmFest_Bridging_Header_h
+#define PTFilmFest_PTFilmFest_Bridging_Header_h
+
+
+#endif
